3a0dd47de6a8644b6113f985cae6c56e3b6cecb6
yossshhhi <arailim777@mail.ru>
2023-03-11 14:40:12 +0600

    Add task Human readable time

---
+++ src/main/java/five_kyu/HumanReadableTime.java
@@ -0,0 +1,24 @@
+package five_kyu;
+
+public class HumanReadableTime {
+
+    // Write a function, which takes a non-negative integer (seconds) as input
+    // and returns the time in a human-readable format (HH:MM:SS)
+    // HH = hours, padded to 2 digits, range: 00 - 99
+    // MM = minutes, padded to 2 digits, range: 00 - 59
+    // SS = seconds, padded to 2 digits, range: 00 - 59
+    // The maximum time never exceeds 359999 (99:59:59)
+    // You can find some examples in the test fixtures.
+
+    public static String makeReadable(int seconds) {
+        return String.format("%02d:%02d:%02d", seconds / 3600, (seconds % 3600) / 60, (seconds % 60));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeReadable(359999));
+        System.out.println(makeReadable(60));
+        System.out.println(makeReadable(86399));
+        System.out.println(makeReadable(5));
+        System.out.println(makeReadable(0));
+    }
+}
