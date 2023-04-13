99461fe698282b920c34f1bc8f8be92a97745e4e
yossshhhi <arailim777@mail.ru>
2023-04-13 18:47:27 +0600

    Add task Who likes it?

---
+++ src/main/java/six_kyu/WhoLikesIt.java
@@ -0,0 +1,16 @@
+package six_kyu;
+
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+public class WhoLikesIt {
+
+    public static String whoLikesIt(String... names) {
+        switch (names.length) {
+            case 0 : return "no one likes this";
+            case 1 : return names[0] + " likes this";
+            case 2 : return names[0] + " and "+ names[1] + " like this";
+            case 3 : return names[0] + ", " + names[1] + " and "+ names[2] + " like this";
+        }
+        return names[0] + ", " + names[1] + " and "+ (names.length - 2) + " others like this";
+    }
+}
